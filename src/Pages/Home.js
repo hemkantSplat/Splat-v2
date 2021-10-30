a6815413b2ebd9125a4b9b89e09c17684b7fe9ef
Dv <dv@Dvs-MacBook-Air.local>
2021-10-30 16:03:31 +0530

    fixes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,19 @@
-import React from "react";
-import "./Home.css";
+import React from 'react'
+import './Home.css'
+import Video from '../Assets/bg2.mp4'
+
 const Home = () => {
   return (
-    <div className="container home">
-      <div className="container-center home-center">
-        <h1>Home</h1>
+    <div className='container home'>
+      <video autoPlay muted loop id='myVideo'>
+        <source src={Video} type='video/mp4' />
+      </video>
+      <div className='gradient'></div>
+      <div className='container-center home-center'>
+        <h1 className='hero-title'>Home</h1>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
