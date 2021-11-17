ffab98839a2d7615a9e8194b6f2520bcffede058
Dv <dv@Dvs-MacBook-Air.local>
2021-11-17 15:44:22 +0530

    pwa

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,9 +14,89 @@ const Home = () => {
         transition={{ ease: 'easeIn', duration: 0.5 }}
         exit={{ opacity: 0, duration: 0.5 }}
       >
+        {/* <div id='video1'>
+          <div
+            style={{
+              position: 'absolute',
+              zIndex: 1,
+              inset: '0px',
+              overflow: 'hidden',
+              backgroundSize: 'cover',
+              backgroundColor: 'transparent',
+              backgroundImage: 'none',
+              backgroundPosition: '50% 50%',
+              backgroundRepeat: 'no-repeat',
+            }}
+          >
+            <video
+              autoPlay
+              muted
+              loop
+              style={{
+                margin: 'auto',
+                position: 'absolute',
+                zIndex: 1,
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                visibility: 'visible',
+                opacity: 1,
+                width: '100vw',
+                height: 'auto',
+              }}
+            >
+              <source src={Video} type='video/mp4' />
+            </video>
+          </div>
+        </div>
+        <div id='video2'>
+          <div
+            style={{
+              position: 'absolute',
+              zIndex: 1,
+              top: '0px',
+              left: '0px',
+              bottom: '0px',
+              right: '0px',
+              overflow: 'hidden',
+              backgroundSize: 'cover',
+              backgroundColor: 'transparent',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: '50% 50%',
+              backgroundImage: 'none',
+            }}
+          >
+            <video
+              autoPlay
+              muted
+              loop
+              style={{
+                margin: 'auto',
+                position: 'absolute',
+                zIndex: 1,
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                visibility: 'visible',
+                opacity: 1,
+                width: 'auto',
+                height: '502px',
+                borderRadius: '50%',
+              }}
+            >
+              <source src={Video} type='video/mp4' />
+            </video>
+          </div>
+        </div> */}
         <video autoPlay muted loop id='myVideo'>
           <source src={Video} type='video/mp4' />
         </video>
+        {/* <video autoPlay muted loop id='myVideo2'>
+          <source
+            src='https://monopo.co.jp/wp-content/themes/monopo/video/circle4.mp4'
+            type='video/mp4'
+          />
+        </video> */}
         <div className='gradient'></div>
         <div className='container-center home-center'>
           <h1 className='hero-title'>Home</h1>
