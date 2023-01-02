792e409b72172c0f6bec30900a47fba29439f614
kalam <syed.kalam@ncplinc.com>
2023-01-02 16:39:49 +0530

    Deleted files

--- src/Client.js
+++
@@ -1,8 +0,0 @@
-import sanityClient from "@sanity/client";
-
-export default sanityClient({
-    projectId: "lir6j5mc",
-    dataset: "production",
-    useCdn: true,
-    apiVersion: new Date().toISOString().slice(0, 10)
-})
